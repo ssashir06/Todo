c11809d23c410730fe51b1b9312ed8f0dade8d45
Hiroshi Takeda <takejaponpon+git@gmail.com>
2014-06-15 21:34:39 +0900

    set Parse settings

--- js/todos.js
+++ js/todos.js
@@ -7,8 +7,8 @@ $(function() {
   Parse.$ = jQuery;
 
   // Initialize Parse with your Parse application javascript keys
-  Parse.initialize("your-application-id",
-                   "your-javascript-key");
+  Parse.initialize("KYi7CA33WPiiRlVo3wip8N8mglnwtbBDHrrnHu8s",
+                   "rb4n4Xj3R13497GfPNwqVskRioqr80ypw47L5se1");
 
   // Todo Model
   // ----------
@@ -404,4 +404,39 @@ $(function() {
   new AppRouter;
   new AppView;
   Parse.history.start();
+
+  window.fbAsyncInit = function() {
+    Parse.FacebookUtils.init({
+      appId      : '1496372537247600',
+      channelUrl : '//hujiko.sakura.ne.jp/test/ParseTodo/channel.html',
+      status     : true,
+      xfbml      : true
+    });
+
+    FB.getLoginStatus(function(response) {
+      if (response.status !== 'connected') {
+        Parse.FacebookUtils.logIn(null, {
+          error: function(user, error) {
+            alert("User cancelled the Facebook login or did not fully authorize.");
+          }
+        });
+      }
+    });
+
+    FB.Event.subscribe('auth.authResponseChange', function(response) {
+      if (response.status === 'connected') {
+        if (!Parse.User.current()) {
+          Parse.FacebookUtils.logIn(null, {
+            success: function(user) {
+              new ManageTodosView;
+            }
+          });
+        }
+      } else {
+        new LogInView;
+      }
+    });
+
+  };
 });
+
